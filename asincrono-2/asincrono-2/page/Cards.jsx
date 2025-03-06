2b46bdc6f3edb6a6ec5bd8985a765a129fcde8a9
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-03-06 12:40:24 +0100

    init asincrono 2

---
+++ asincrono-2/asincrono-2/page/Cards.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const cardsData = [
+    { id: 1, name: "Antonio Ruiz", message: "Felicidades amigo. Te veo pronto", time: "08:00" },
+    { id: 2, name: "Manuela Antonia", message: "Hola, un placer hablar contigo", time: "18:33" },
+    { id: 3, name: "Angela H.", message: "Que mal dia ha salido hoy", time: "13:40" },
+    { id: 4, name: "Javiera Dominguez", message: "Ayer no pude ir al encuentro porque me puse enfermo", time: "10:00" }
+];
+
+const Cards = () => {
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handleNextCard = () => {
+        if (activeIndex < cardsData.length - 1) {
+            setActiveIndex(activeIndex + 1)
+        }
+    }
+
+    return (
+
+        <div className="Card-stack">
+            {cardsData.map((card, i) => (
+                <div key={card.id}
+                    className={`card ${i == activeIndex ? "card-front" : "card-back"}`}
+                    style={{ zIndex: cardsData.length - i }}
+                    onClick={handleNextCard}
+                >
+                    <h2>Introductions</h2>
+                    <div className="Profile">
+                        <h3>{card.name}</h3>
+                        <span>{card.time}</span>
+                    </div>
+                    <p>{card.message}</p>
+
+
+                </div>
+            ))}
+
+        </div>
+    );
+}
+
+export default Cards;
---
+++ asincrono-2/asincrono-2/src/App.jsx
@@ -0,0 +1,13 @@
+import Cards from "../page/Cards"
+import './css/Cards.css'
+
+function App() {
+
+  return (
+    <>
+      <Cards/>
+    </>
+  )
+}
+
+export default App
