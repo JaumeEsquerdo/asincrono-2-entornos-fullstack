affc76194bd81bf2bf9250f0af55202449770f02
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-03-14 17:09:47 +0100

    layout triage

--- asincrono-2/asincrono-2/page/TriageWeb.jsx
+++ asincrono-2/asincrono-2/page/TriageWeb.jsx
@@ -1,17 +1,150 @@
-import { useState } from "react";
+import {Link} from 'react'
 
 const TriageWeb = () => {
-    return ( 
+    return (
         <div className="TriageWeb">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <header className="TriageWeb-header">
+                <div>
+                    <h1 className="">Triage is first aid for your inbox.</h1>
+                    <p>Everything you loved about the
+                        original Triage is back – only better.
+                        Download Triage 2 on the App Store
+                        today.</p>
+                    <img src="" alt="Boton" />
+                </div>
+                <img src="" alt="App" />
+
+            </header>
+            <section>
+                <img src="" alt="" />
+
+                <div>
+                    <img src="" alt="" />
+                    <h2>Drag left to archive</h2>
+                    <p>Unread messages appear
+                        in a stack of cards, like
+                        this.</p>
+                </div>
+
+            </section>
+            <section>
+                <div>
+                    <img src="" alt="" />
+                    <h2>Drag right to keep
+                    </h2>
+                    <p>Or create your own custom workflow.
+                    </p>
+                </div>
+                <img src="" alt="" />
+
+
+            </section>
+            <section>
+                <img src="" alt="" />
+
+                <div>
+                    <img src="" alt="" />
+                    <h2>Tap to expand</h2>
+                    <p>Open the message to
+                    view the whole thread.</p>
+                </div>
+
+            </section>
+            <section>
+                <div>
+                    <img src="" alt="" />
+                    <h2>Reply in context
+
+                    </h2>
+                    <p>Send a quick reply without leaving the
+                    app.
+                    </p>
+                </div>
+                <img src="" alt="" />
+
+
+            </section>
+
+            <section>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <h3></h3>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <h3></h3>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <h3></h3>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <h3></h3>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <h3></h3>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <h3></h3>
+                </div>
+            </section>
+
+            <footer>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+
+                <nav>
+                    <Link>FAQ</Link>
+                    <Link>Privacy</Link>
+                    <Link>Terms</Link>
+                    <Link>Security</Link>
+
+                    <div>
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
+                </nav>
+
+            </footer>
         </div>
-     );
+    );
 }
- 
+
 export default TriageWeb;
