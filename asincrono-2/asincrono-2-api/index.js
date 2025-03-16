fe9857a952049ae753ddb72bc622c4a9e36eb5b3
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-03-16 12:28:50 +0100

    manejador de errores

--- asincrono-2/asincrono-2-api/index.js
+++ asincrono-2/asincrono-2-api/index.js
@@ -10,6 +10,9 @@ import router from './routes/index.routes.js'
 
 const app = express()
 
+console.clear(); //limpiar terminal aal reiniciar proyecto
+
+
 app.use(cors()); // conectar desde cualquier conexión
 app.use(express.json()); // leer datos que vienen en el body de mi req
 app.use(express.urlencoded({extended:true})); // leer datos de un form html
@@ -20,13 +23,21 @@ conectarDB();
 
 //rutas front
 
-console.clear(); //limpiar terminal aal reiniciar proyecto
-
 app.get("/", (req,res)=>{
     res.send('Asíncrono 2 con mongo')
 })
 
-app.use("/api/v1", router)
+app.use("/api/v1", router);
+
+
+// se sabe q es el manejador de errores porq tiene 4 parametros
+app.use((err, req, res, next)=>{
+    console.error('error en la API:', err)
+    res.status(500).json({
+        status:'error',
+        message: err.message ||'error interno del servidor'
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log(`servidor funcionando en ${DOMAIN}:${PORT}`)
--- asincrono-2/asincrono-2/page/Cards.jsx
+++ asincrono-2/asincrono-2/page/Cards.jsx
@@ -1,185 +1,185 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-const cardsData = [
-    {
-        id: 1, name: "Antonio Ruiz", message: "Felicidades amigo. Te veo pronto", time: "08:00",
-        history: [{ sender: "Antonio Ruiz", message: "Felicidades amigo. Te veo pronto", time: "08:00" }, {
-            sender: "Tú", message: "Gracias, nos vemos pronto", time: "08:05"
-        }]
-    },
-    {
-        id: 2, name: "Manuela Antonia", message: "Hola, un placer hablar contigo", time: "18:33",
-        history: [{ sender: "Manuela Antonia", message: "Hola, un placer hablar contigo", time: "18:33" }, {
-            sender: "Tú", message: "Muy interesante la charla", time: "20:05"
-        }]
-    },
-    {
-        id: 3, name: "Angela H.", message: "Que mal dia ha salido hoy", time: "13:40",
-        history: [{ sender: "Angela H.", message: "Que mal dia ha salido hoy", time: "13:40" }, {
-            sender: "Tú", message: "Un dia bastante malo verdad? Nos vemos pronto", time: "14:05"
-        }]
-    },
-];
+// const cardsData = [
+//     {
+//         id: 1, name: "Antonio Ruiz", message: "Felicidades amigo. Te veo pronto", time: "08:00",
+//         history: [{ sender: "Antonio Ruiz", message: "Felicidades amigo. Te veo pronto", time: "08:00" }, {
+//             sender: "Tú", message: "Gracias, nos vemos pronto", time: "08:05"
+//         }]
+//     },
+//     {
+//         id: 2, name: "Manuela Antonia", message: "Hola, un placer hablar contigo", time: "18:33",
+//         history: [{ sender: "Manuela Antonia", message: "Hola, un placer hablar contigo", time: "18:33" }, {
+//             sender: "Tú", message: "Muy interesante la charla", time: "20:05"
+//         }]
+//     },
+//     {
+//         id: 3, name: "Angela H.", message: "Que mal dia ha salido hoy", time: "13:40",
+//         history: [{ sender: "Angela H.", message: "Que mal dia ha salido hoy", time: "13:40" }, {
+//             sender: "Tú", message: "Un dia bastante malo verdad? Nos vemos pronto", time: "14:05"
+//         }]
+//     },
+// ];
 
-export const Cards = () => {
+// export const Cards = () => {
 
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [expanded, setExpanded] = useState(false)
-    const [selectedCard, setSelectedCard] = useState(null)
-    const [touchStartX, setTouchStartX] = useState(null)
-    const [archivedMessages, setArchiveMessages] = useState([])
-    const [exitingCard, setExitingCard] = useState(null)
-    const [exitDirection, setExitDirection] = useState(null) //izq o derecha
-    const [isAnimating, setIsAnimating] = useState(false)
-
-
-    // const handleNextCard = () => {
-    //     if (activeIndex < cardsData.length - 1) {
-    //         setActiveIndex(activeIndex + 1)
-    //     }
-    // }
-
-    // const handleExpand = (card) => {
-    //     setExpanded(prev => !prev)
-    //     setSelectedCard(card)
-    // }
-
-    // capturar el inicio del toque en la pantalla
-    const handleTouchStart = (e) => {
-        setTouchStartX(e.touches[0].clientX);
-    }
-
-    // capturar el final del toque y mirar dirección
-    const handleTouchEnd = (e) => {
-        if (!touchStartX || isAnimating) return; // evita q funcione si ya está en animacion
+//     const [activeIndex, setActiveIndex] = useState(0);
+//     const [expanded, setExpanded] = useState(false)
+//     const [selectedCard, setSelectedCard] = useState(null)
+//     const [touchStartX, setTouchStartX] = useState(null)
+//     const [archivedMessages, setArchiveMessages] = useState([])
+//     const [exitingCard, setExitingCard] = useState(null)
+//     const [exitDirection, setExitDirection] = useState(null) //izq o derecha
+//     const [isAnimating, setIsAnimating] = useState(false)
 
 
-        // obtiene la posición X del dedo en la pantalla que se ha levantado
-        const touchEndX = e.changedTouches[0].clientX;
-        const diff = touchStartX - touchEndX
+//     // const handleNextCard = () => {
+//     //     if (activeIndex < cardsData.length - 1) {
+//     //         setActiveIndex(activeIndex + 1)
+//     //     }
+//     // }
 
-        // si el desliz pasa de 50 (umbral para q actue) y cumple lo otro avanza al hacer desliz a la izq
-        if (diff > 50
-            &&
-            activeIndex < cardsData.length - 1
-        ) {
+//     // const handleExpand = (card) => {
+//     //     setExpanded(prev => !prev)
+//     //     setSelectedCard(card)
+//     // }
 
-            const archiveCard = cardsData[activeIndex]
+//     // capturar el inicio del toque en la pantalla
+//     const handleTouchStart = (e) => {
+//         setTouchStartX(e.touches[0].clientX);
+//     }
 
-            setIsAnimating(true) //bloquea interacción
-            setExitingCard(archiveCard.id);
-            setExitDirection("left")
-
-            setTimeout(() => {
-                setArchiveMessages(prev => [...prev, archiveCard])
-                setActiveIndex(prev => prev + 1)
-                setExitingCard(null)
-                setExitDirection(null)
-                setIsAnimating(false)
-            }, 500) //tiempo de espera  a ejecutar
+//     // capturar el final del toque y mirar dirección
+//     const handleTouchEnd = (e) => {
+//         if (!touchStartX || isAnimating) return; // evita q funcione si ya está en animacion
 
 
-        } else if (diff < -50 && activeIndex < cardsData.length - 1) {
-            if (isAnimating) return;
+//         // obtiene la posición X del dedo en la pantalla que se ha levantado
+//         const touchEndX = e.changedTouches[0].clientX;
+//         const diff = touchStartX - touchEndX
 
-            // al deslizar a la derecha avanza solamente
-            setExitingCard(cardsData[activeIndex].id)
-            setExitDirection("right")
-            setIsAnimating(true)
+//         // si el desliz pasa de 50 (umbral para q actue) y cumple lo otro avanza al hacer desliz a la izq
+//         if (diff > 50
+//             &&
+//             activeIndex < cardsData.length - 1
+//         ) {
 
-            setTimeout(() => {
-                setActiveIndex(prev => prev + 1)
-                setExitingCard(null)
-                setExitDirection(null)
-                setIsAnimating(false)
+//             const archiveCard = cardsData[activeIndex]
 
-            }, 500)
-        }
+//             setIsAnimating(true) //bloquea interacción
+//             setExitingCard(archiveCard.id);
+//             setExitDirection("left")
 
-        //devolver la posición al levantar los dedos a null
-        setTouchStartX(null)
-    }
+//             setTimeout(() => {
+//                 setArchiveMessages(prev => [...prev, archiveCard])
+//                 setActiveIndex(prev => prev + 1)
+//                 setExitingCard(null)
+//                 setExitDirection(null)
+//                 setIsAnimating(false)
+//             }, 500) //tiempo de espera  a ejecutar
 
 
-    const handleCardClick = (card, i) => {
-        if (selectedCard?.id !== card.id) {
-            setExpanded(true);
-            setSelectedCard(card);
-            setActiveIndex(i);
-        } else {
-            setExpanded(false)
-            setSelectedCard(null) // para que se pueda volver a abrir
-        }
-    };
+//         } else if (diff < -50 && activeIndex < cardsData.length - 1) {
+//             if (isAnimating) return;
 
-    const resetCards = () => {
-        setActiveIndex(0);
-        setSelectedCard(null);
-        setExpanded(false);
-    }
+//             // al deslizar a la derecha avanza solamente
+//             setExitingCard(cardsData[activeIndex].id)
+//             setExitDirection("right")
+//             setIsAnimating(true)
 
-    return (
-        <main className="Main">
-            <h2>Desliza a la izquierda para avanzar</h2>
-            <div className="Card-stack"
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-            >
-                {/* filtrar primero las cartas archivas antes del map (si la carta no está archivada sera true y las enseñara) */}
-                {cardsData.filter(card => !archivedMessages.some(archived => archived.id == card.id)).map((card, i) => (
-                    i >= activeIndex && (
-                        <div onClick={() => handleCardClick(card, i)} key={card.id}
-                            className={`Card ${i === activeIndex ? "Card-front" : "Card-back"} ${exitingCard == card.id ? `Exiting-${exitDirection}` : ""}`}
+//             setTimeout(() => {
+//                 setActiveIndex(prev => prev + 1)
+//                 setExitingCard(null)
+//                 setExitDirection(null)
+//                 setIsAnimating(false)
 
-                            style={{ zIndex: cardsData.length - i }}
-                        >
-                            <h2 className="Card-h2">Introductions</h2>
-                            <div className="Profile">
-                                <h3 className="Profile-h3">{card.name}</h3>
-                                <span>{card.time}</span>
-                            </div>
-                            <p>{card.message}</p>
-                        </div>
-                    )
-                ))}
-            </div>
+//             }, 500)
+//         }
 
-            {expanded && <CardAbierta card={selectedCard} setExpanded={setExpanded} />}
+//         //devolver la posición al levantar los dedos a null
+//         setTouchStartX(null)
+//     }
 
-            <button onClick={resetCards}>Reiniciar</button>
-        </main>
 
-    );
-}
+//     const handleCardClick = (card, i) => {
+//         if (selectedCard?.id !== card.id) {
+//             setExpanded(true);
+//             setSelectedCard(card);
+//             setActiveIndex(i);
+//         } else {
+//             setExpanded(false)
+//             setSelectedCard(null) // para que se pueda volver a abrir
+//         }
+//     };
 
-export const CardAbierta = ({ card, setExpanded }) => {
+//     const resetCards = () => {
+//         setActiveIndex(0);
+//         setSelectedCard(null);
+//         setExpanded(false);
+//     }
 
-    const [replyMessage, setReplyMessage] = useState("");
+//     return (
+//         <main className="Main">
+//             <h2>Desliza a la izquierda para avanzar</h2>
+//             <div className="Card-stack"
+//                 onTouchStart={handleTouchStart}
+//                 onTouchEnd={handleTouchEnd}
+//             >
+//                 {/* filtrar primero las cartas archivas antes del map (si la carta no está archivada sera true y las enseñara) */}
+//                 {cardsData.filter(card => !archivedMessages.some(archived => archived.id == card.id)).map((card, i) => (
+//                     i >= activeIndex && (
+//                         <div onClick={() => handleCardClick(card, i)} key={card.id}
+//                             className={`Card ${i === activeIndex ? "Card-front" : "Card-back"} ${exitingCard == card.id ? `Exiting-${exitDirection}` : ""}`}
 
-    const handleReplyMessage = (e) => {
-        setReplyMessage(e.target.value);
-    }
-    return (
-        <div className="CardAbierta">
-            <div className="Profile"></div>
-            <h2>{card.name}</h2>
-            <button onClick={() => setExpanded(false)}>X</button>
-            <div className="Conversation">
-                {card.history.map((msg, i) => (
-                    <div key={i}>
-                        <p><strong>{msg.sender}</strong></p>
-                        <p>{msg.message}</p>
-                        <span>{msg.time}</span>
-                    </div>
-                ))}
-            </div>
+//                             style={{ zIndex: cardsData.length - i }}
+//                         >
+//                             <h2 className="Card-h2">Introductions</h2>
+//                             <div className="Profile">
+//                                 <h3 className="Profile-h3">{card.name}</h3>
+//                                 <span>{card.time}</span>
+//                             </div>
+//                             <p>{card.message}</p>
+//                         </div>
+//                     )
+//                 ))}
+//             </div>
 
-            {/* input para el nuevo mensaje del reply */}
-            <div className="ReplySection">
-                <textarea value={replyMessage} onChange={handleReplyMessage} rows={10} cols={50} placeholder="Escribe tu respuesta..."/>
-                <button>Enviar</button>
-            </div>
-        </div>
-    );
-}
+//             {expanded && <CardAbierta card={selectedCard} setExpanded={setExpanded} />}
+
+//             <button onClick={resetCards}>Reiniciar</button>
+//         </main>
+
+//     );
+// }
+
+// export const CardAbierta = ({ card, setExpanded }) => {
+
+//     const [replyMessage, setReplyMessage] = useState("");
+
+//     const handleReplyMessage = (e) => {
+//         setReplyMessage(e.target.value);
+//     }
+//     return (
+//         <div className="CardAbierta">
+//             <div className="Profile"></div>
+//             <h2>{card.name}</h2>
+//             <button onClick={() => setExpanded(false)}>X</button>
+//             <div className="Conversation">
+//                 {card.history.map((msg, i) => (
+//                     <div key={i}>
+//                         <p><strong>{msg.sender}</strong></p>
+//                         <p>{msg.message}</p>
+//                         <span>{msg.time}</span>
+//                     </div>
+//                 ))}
+//             </div>
+
+//             {/* input para el nuevo mensaje del reply */}
+//             <div className="ReplySection">
+//                 <textarea value={replyMessage} onChange={handleReplyMessage} rows={10} cols={50} placeholder="Escribe tu respuesta..."/>
+//                 <button>Enviar</button>
+//             </div>
+//         </div>
+//     );
+// }
 
